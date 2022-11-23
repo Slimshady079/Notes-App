abe7cbeebf73d6ad3eb6719841b320c21bb8b383
max <maximiliangibes@gmail.com>
2022-11-23 16:04:56 -0700

    added correct file structure and created local server. Has access to apiRoutes. Has access to both htmls.

---
+++ routes/apiRoutes.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+let store = require("../public/assets/resources/db.json");
+console.log("data: ", store);
+// router.get("/notes", (req, res) => {});
+
+// module.exports = router;
