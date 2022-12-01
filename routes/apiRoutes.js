75f94b65536b8bc042eb1907632c2947ea1fe81c
max <maximiliangibes@gmail.com>
2022-11-30 20:12:49 -0700

    adds

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,36 @@
 const router = require("express").Router();
 let store = require("../public/assets/resources/db.json");
+const { writeFile } = require("fs");
 console.log("data: ", store);
-// router.get("/notes", (req, res) => {});
+
+router.get("/notes", (req, res) => res.json(store));
+
+router.post("/notes", ({ body }, res) => {
+  body.id = Math.floor((1 + Math.random()) * 0x10000).toString(16);
+
+  store.push(body);
+
+  writeFile(
+    "./public/assets/resources/db.json",
+    JSON.stringify(store),
+    (err) => {
+      if (err) throw err;
+      res.json(store);
+    }
+  );
+});
+
+router.delete("/notes/:id", (req, res) => {
+  console.log(req.params);
+  store = store.filter((obj) => obj.id != req.params.id);
+  writeFile(
+    "./public/assets/resources/db.json",
+    JSON.stringify(store),
+    (err) => {
+      if (err) throw err;
+      res.json(store);
+    }
+  );
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const path = require("path");
+const { execArgv } = require("process");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "public")));
 
